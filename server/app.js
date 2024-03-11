d4c8b4ba72ef3fb8284757c276434f3eb0e7e492
annika <annika.sonne@du.edu>
2024-03-11 15:48:44 -0600

    bug fix - server directories

--- server/app.js
+++ server/app.js
@@ -1,12 +1,17 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import path from 'path';
+import bodyParser from 'body-parser';
+import { fileURLToPath } from 'url';
+
 dotenv.config(); 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 import recipeRoutes from './api/recipes/recipes-routes.js';
 import userRoutes from './api/users/users-routes.js';
 import reviewRoutes from './api/reviews/reviews-routes.js';
-import bodyParser from 'body-parser';
 
 export default(port, dbUrl) => {
     mongoose.connect(dbUrl)
@@ -29,9 +34,9 @@ export default(port, dbUrl) => {
     app.use(`/api/recipes/:recipeId/reviews`, reviewRoutes); 
 
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'public', 'index.html'));
-      });
-      
+        res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+    });
+
     app.listen(port, () => {
         console.log(`App started on port ${port}`);
     });
